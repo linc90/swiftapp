7153019c2bfcc8db39080b9f630bbbbab0a4ee20
Ligaya & Lincoln Distler <linc90@Lincolns-MacBook.local>
2017-03-31 20:47:45 -0400

    Initial Commit

---
+++ Swift App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift App
+//
+//  Created by Ligaya & Lincoln Distler on 3/31/17.
+//  Copyright © 2017 Lincoln. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
