a8afaeea46cea4b4490fceed2d39aababb07ce19
lincolndistler <linc90@icloud.com>
2017-04-04 20:08:07 -0400

    first commit

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -9,10 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var theLabel: UILabel!
+    
+    var tapCount = 0
 
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        tapCount = tapCount + 1
+        if tapCount >= 10 {
+            theLabel.text = "You have tapped the button 10 times!"
+        }
+        
+        // used print to learn about a useful debugging feature called print
+        
+        
+    }
+   
+    @IBAction func buttonExercise(_ sender: Any) {
+        
+        theLabel.text = "Buttons are cool."
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
